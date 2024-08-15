c9f8dc2061a6933fca85cccd51b5f59b1abc15a2
nanaji3850 <169175513+nanaji3850@users.noreply.github.com>
2024-08-15 12:56:03 +0000

    2 user routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const {users,user} = require("./data/users.json");
+const {users} = require("./data/users.json");
 
 const app = express();
 
@@ -9,7 +9,7 @@ const PORT = 8081;
 app.use(express.json());
 
 /**
- * Routes: /users
+ * Routers: /users
  * Methods: GET
  * Description: Get all users
  * Access: public
@@ -19,9 +19,109 @@ app.use(express.json());
 app.get("/users", (req,res) => {
     res.status(200).json({
         success: true,
-        data:users,user,
+        data:users,
     });
 });
+
+/**
+ * Router: /users/:id
+ * Methods: GET
+ * Description: Get one specific User
+ * Access: Piblic
+ * Parameters: None
+ */
+
+app.get("/users/:id", (req,res) => {
+    const {id} = req.params;
+    const user = users.find((each) => each.id == id);
+    if (!user){
+        return res.status(404).json({
+            sucess:false,
+            message:"user not found",
+        })
+    }
+    return res.status(200).json({
+        sucess:true,
+        message:"user found",
+        data:user,
+    })
+})
+
+/**
+ * Router: /users
+ * Methods: POST
+ * Description: Add new user
+ * Access: Piblic
+ * Parameters: None
+ */
+
+app.post("/users", (req,res) => {
+    const {id,name,surname,issuedbook,issueddate,returndate,subscriptiontype,subscriptiondate} = req.body;
+    const user = users.find((each) => each.id == id);
+    if(user){
+        return res.status(404).json({
+            sucess: false,
+            message: "user already there"
+        })
+    }
+    users.push({
+        id,
+        name,
+        surname,
+        issuedbook,
+        issueddate,
+        returndate,
+        subscriptiontype,
+        subscriptiondate,
+    });
+
+    return res.status(201).json({
+        sucess: true,
+        message: "user added succesfully",
+        data: users,
+    })
+})
+
+/**
+ * Router: /users/:id
+ * Methods: PUT
+ * Description: update user by id
+ * Access: Piblic
+ * Parameters: id
+ */
+app.put("/users/:id", (req,res) => {
+    const {id} = req.params;
+    const {data} = req.body;
+
+    const user = users.find((each) => each.id ==id);
+    if(!user){
+        return res.status(404).json({
+            sucess:false,
+            message:"user not found",
+        });
+    }
+    const updatesuerdata = users.map((each) => {
+        if (each.id ==id ){
+            return {
+                ...each,
+                ...data,
+            };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        sucess:true,
+        message:"user updated",
+        udate:updatesuerdata,
+    })
+
+    });
+
+ 
+
+
+
 app.get("/",(req,res) => {
     res.status(200).json({
         message: "Server is up and running :-)",
